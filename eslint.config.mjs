59927211cdaf91a1ece4977e4bf6fbce5fd46908
Maurizio Turatti <maurizio@softinstigate.com>
2024-07-03 18:46:01 +0200

    chore: Update eslint.config.mjs to use ES modules and enable 'no-unused-vars' warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,12 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 
 export default [
-    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
     { languageOptions: { globals: globals.node } },
     pluginJs.configs.recommended,
+    {
+        rules: {
+            'no-unused-vars': 'warn',
+        },
+    },
 ]
